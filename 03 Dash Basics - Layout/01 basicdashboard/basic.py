11ae217518bb66a174d3a360b3fa0c49e4091cd2
Eliud Kagema <eliud.k.maina@gmail.com>
2020-02-04 21:28:32 +0300

    Basic Dashboard Layout

---
+++ 03 Dash Basics - Layout/01 basicdashboard/basic.py
@@ -0,0 +1,16 @@
+import dash		
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+app = dash.Dash()
+
+app.layout = html.Div(children = [
+			html.H1("Basic Dashboard")
+	])
+
+
+
+if __name__ == '__main__':
+
+	app.run_server()
