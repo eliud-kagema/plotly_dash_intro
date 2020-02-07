ec017db6a13992e82de7460ead0d67295da1837d
Eliud Kagema <eliud.k.maina@gmail.com>
2020-02-07 23:05:48 +0300

    Inner Div

--- 04 Dashboard Components/html_components.py
+++ 04 Dashboard Components/html_components.py
@@ -3,7 +3,9 @@ import dash_html_components as html
 
 app = dash.Dash()
 
-app.layout = html.Div(['This is the Outer Most Div'],
+app.layout = html.Div(['This is the Outer Most Div',
+                            html.Div(['This is an inner Div'], 
+                            style={'color':'red'})],    
                         style={'color':'green', 'border': '2px green solid'}
 
 
