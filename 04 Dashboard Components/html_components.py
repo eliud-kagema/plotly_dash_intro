b7bfecd8c7ed27cbfd94f71de29052a2afb31abb
Eliud Kagema <eliud.k.maina@gmail.com>
2020-02-07 23:01:29 +0300

    Div

--- 04 Dashboard Components/html_components.py
+++ 04 Dashboard Components/html_components.py
@@ -3,9 +3,11 @@ import dash_html_components as html
 
 app = dash.Dash()
 
-app.Layout = html.Div([
-    'This is the Outer Most Div'
-])
+app.layout = html.Div(['This is the Outer Most Div'],
+                        style={'color':'green', 'border': '2px green solid'}
+
+
+)
 
 if __name__ == '__main__':
     app.run_server()
