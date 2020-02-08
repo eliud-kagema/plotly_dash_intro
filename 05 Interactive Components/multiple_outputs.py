5a8c17daca8fc70be1a912a7a16d8072a72710ee
Eliud Kagema <eliud.k.maina@gmail.com>
2020-02-08 21:18:46 +0300

    multiple outputs

--- 05 Interactive Components/multiple_outputs.py
+++ 05 Interactive Components/multiple_outputs.py
@@ -8,4 +8,42 @@ import pandas as pd
 
 df = df = pd.read_csv('../data/wheels.csv')
 
-print(df.head())
+app = dash.Dash()
+
+app.layout = html.Div([
+
+            dcc.RadioItems(id='wheels', 
+                            options=[{'label': i, 'value': i} for i in df['wheels'].unique()],
+                            value=1
+            ),
+
+            html.Div(id='wheels-output'),
+
+            html.Hr(),
+
+            dcc.RadioItems(id='colors', 
+                            options=[{'label': i, 'value': i} for i in df['color'].unique()],
+                            value='blue'
+
+            ),
+            html.Div(id='colors-output'),
+
+], style={'fontFamily': 'helvetica', 'fontSize': 15})
+
+
+@app.callback(Output('wheels-output', 'children'),
+            [Input('wheels', 'value')])
+def callback_a(wheels_value):
+    return "You choose {}".format(wheels_value)
+
+
+@app.callback(Output('colors-output', 'children'),
+            [Input('colors', 'value')]
+
+)
+def callback_b(colors_value):
+    return "You choose {}".format(colors_value)
+
+
+if __name__ == '__main__':
+    app.run_server()
