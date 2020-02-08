8a943ddbaf9bdd9ac16246553cf427efd9778ae8
Eliud Kagema <eliud.k.maina@gmail.com>
2020-02-08 18:51:17 +0300

    core componets

--- 04 Dashboard Components/core_compoments.py
+++ 04 Dashboard Components/core_compoments.py
@@ -5,16 +5,51 @@ import dash_core_components as dcc
 app = dash.Dash()
 
 app.layout = html.Div([
-            html.Label('Dropdown'),
-            dcc.Dropdown(options=[
-                                    {'label': 'New York City', 'value': 'NYC'},
-                                    {'label': 'San Francisco', 'value': 'SF'}],
-                                    
-                        value='SF'),
 
-            html.Label('Slider'),
-            dcc.Slider(min=-10, max=10, step=0.5, value=0)
-])
+    # DROPDOWN https://dash.plot.ly/dash-core-components/dropdown
+    html.Label('Dropdown'),
+    dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': 'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='MTL'
+    ),
+
+    html.Label('Multi-Select Dropdown'),
+    dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value=['MTL', 'SF'],
+        multi=True
+    ),
+
+    # SLIDER https://dash.plot.ly/dash-core-components/slider
+    html.Label('Slider'),
+    html.P(
+    dcc.Slider(
+        min=-5,
+        max=10,
+        step=0.5,
+        marks={i: i for i in range(-5,11)},
+        value=-3
+    )),
+
+    # RADIO ITEMS https://dash.plot.ly/dash-core-components/radioitems
+    html.Label('Radio Items'),
+    dcc.RadioItems(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': 'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='MTL'
+    )
+], style={'width': '50%'})
 
 if __name__ == '__main__':
     app.run_server()
