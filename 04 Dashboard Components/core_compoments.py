8818aab3ff83485b1caf428dcfa950026bb73e07
Eliud Kagema <eliud.k.maina@gmail.com>
2020-02-08 18:39:43 +0300

    core componets

--- 04 Dashboard Components/core_compoments.py
+++ 04 Dashboard Components/core_compoments.py
@@ -4,5 +4,17 @@ import dash_core_components as dcc
 
 app = dash.Dash()
 
+app.layout = html.Div([
+            html.Label('Dropdown'),
+            dcc.Dropdown(options=[
+                                    {'label': 'New York City', 'value': 'NYC'},
+                                    {'label': 'San Francisco', 'value': 'SF'}],
+                                    
+                        value='SF'),
+
+            html.Label('Slider'),
+            dcc.Slider(min=-10, max=10, step=0.5, value=0)
+])
+
 if __name__ == '__main__':
     app.run_server()
