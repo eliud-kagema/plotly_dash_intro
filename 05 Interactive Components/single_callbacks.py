956f76f3d9fc69e239eae523c09a4cbf04ebcaac
Eliud Kagema <eliud.k.maina@gmail.com>
2020-02-08 19:13:29 +0300

    single interactivity

--- 05 Interactive Components/single_callbacks.py
+++ 05 Interactive Components/single_callbacks.py
@@ -7,10 +7,17 @@ from dash.dependencies import Input, Output
 app = dash.Dash()
 
 app.layout = html.Div([
+
     dcc.Input(id='my_id', value='Initial text', type='text'),
     html.Div(id='my-div')
 ])
 
+@app.callback(Output(component_id='my-div', component_property='children'),
+                [Input(component_id='my-id', component_property='value')])
+
+
+def update_output_div(input_value):
+    return "You entered: {}".format(input_value)
 
 if __name__ == '__main__':
     app.run_server()
